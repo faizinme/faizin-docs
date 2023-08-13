6ba2222a2394b9d535625815297c39cc407b1c57
N Faizin <faizin.apps@gmail.com>
2023-08-13 18:17:30 +0700

    add svg

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Tentang saya',
-    Svg: require('@site/static/img/employee-svgrepo-com.svg').default,
+    Svg: require('@site/static/img/web-dev.svg').default,
     description: (
       <>
         Terkadang, hal <code>penting</code> terlupakan. Blog ini mencatat apa yang penulis anggap berharga, menjaga agar tak hilang dalam aliran waktu.
