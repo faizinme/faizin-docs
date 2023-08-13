0fa92be0c295e20585a6ec32ae9a811f32bb9e1b
N Faizin <faizin.apps@gmail.com>
2023-08-13 23:04:36 +0700

    update svg

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Tentang saya',
-    Svg: require('@site/static/img/web-dev-140.svg').default,
+    Svg: require('@site/static/img/about.svg').default,
     description: (
       <>
         Terkadang, hal <code>penting</code> terlupakan. Blog ini mencatat apa yang penulis anggap berharga, menjaga agar tak hilang dalam aliran waktu.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Dokumentasi',
-    Svg: require('@site/static/img/folder-settings-svgrepo-com.svg').default,
+    Svg: require('@site/static/img/docs.svg').default,
     description: (
       <>
         Personal website ini diciptakan sebagai wadah pribadi untuk mendokumentasikan dan memudahkan dalam pekerjaan melalui
