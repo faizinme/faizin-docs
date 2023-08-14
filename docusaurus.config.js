c9e27148f1edcbcdd0de8e203ea5d64c3f1f1dcd
N Faizin <faizin.apps@gmail.com>
2023-08-14 22:23:39 +0700

    add repo url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
           {to: '/about', label: 'About', position: 'left'},
           {to: '/contact', label: 'Contact', position: 'left'},
           {
-            href: 'https://github.com/faizinme',
+            href: 'https://github.com/faizinme/faizin-docs',
             label: 'GitHub',
             position: 'right',
             classname: 'header-github-link',
